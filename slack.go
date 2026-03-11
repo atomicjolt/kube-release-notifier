2d3a8e4cedb2574aa4d3c1d414393dee0bcb00f0
David Spencer <david.spencer@atomicjolt.com>
2026-03-11 18:01:58 -0400

    Make Slack notification message more compact

--- slack.go
+++ slack.go
@@ -8,43 +8,12 @@ import (
 )
 
 func notifySlack(name string, namespace string, environment string, tag string, oldtag string, slackmoji string, tagMessage string) string {
-	fallback := fmt.Sprintf("Deployed %s %s %s", name, environment, tag)
 	api := slack.New(os.Getenv("SLACK_TOKEN"))
-	attachment := slack.Attachment{
-		Pretext:  "New deployment",
-		Fallback: fallback,
-		Fields: []slack.AttachmentField{
-			slack.AttachmentField{
-				Title: "App",
-				Value: name,
-				Short: false,
-			},
-			slack.AttachmentField{
-				Title: "Environment",
-				Value: environment,
-				Short: false,
-			},
-			slack.AttachmentField{
-				Title: "Deployed Version",
-				Value: tag,
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Previous Version",
-				Value: oldtag,
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Build Message",
-				Value: tagMessage,
-				Short: false,
-			},
-		},
-	}
+	text := fmt.Sprintf("%s %s\n*%s*  (was %s)\n%s", name, environment, tag, oldtag, tagMessage)
 
 	channelID, timestamp, err := api.PostMessage(
 		os.Getenv("SLACK_CHANNEL"),
-		slack.MsgOptionAttachments(attachment),
+		slack.MsgOptionText(text, false),
 		slack.MsgOptionIconEmoji(fmt.Sprintf(":%s:", slackmoji)),
 	)
 	if err != nil {
